c91c2bbd8a2c5c61c16fdf70d42fc3f8c738a5e6
chetan <chetan.narian@gmail.com>
2016-07-08 10:00:41 +0530

    #2 - Doesn't work in Mac Safari

--- emoji.js
+++ emoji.js
@@ -3,8 +3,8 @@ var URLEmoji = URLEmoji || {}
 URLEmoji.addEmojiToURL = function () {
   var urlemoji = ['😀', '😬', '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😉', '😊', '🙂', '🙃', '😋', '😌', '😍', '😘', '😗', '😙', '😚', '😜', '😝', '😛', '🤑', '🤓', '😎', '🤗', '😏', '😶', '😐', '😑', '😒', '🙄', '🤔', '😳', '😞', '😟', '😠', '😡', '😔', '😕', '🙁', '😣', '😖', '😫', '😩', '😤', '😮', '😱', '😨', '😰', '😯', '😦', '😧', '😢', '😥', '😪', '😓', '😭', '😵', '😲', '🤐', '😷', '🤒', '🤕', '😴']
 
-  // Only Mac
-  if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
+  // Only Mac Chrome
+  if (/Chrome/.test(navigator.userAgent) && navigator.userAgent.indexOf('Mac OS X') !== -1) {
     var eNumber = Math.floor(Math.random() * (urlemoji.length - 1))
     window.location.hash = urlemoji[eNumber]
   }
